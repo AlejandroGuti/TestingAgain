bdffd52921c6b1593da8ea8666e1123a1751f51e
ALEJANDRO GR <alegut123@hotmail.com>
2022-03-14 17:42:40 -0500

    Finish date class

---
+++ TestingAgain/TestingAgain/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using TestingAgain;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("=================");
+
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2022, 11, 11));
+    Console.WriteLine(new Date(2022, 2, 456));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
