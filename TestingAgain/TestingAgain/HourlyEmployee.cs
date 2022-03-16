b65ed7e480280280f39fbfea665c55e382407a14
ALEJANDRO GR <alegut123@hotmail.com>
2022-03-16 17:42:52 -0500

    HourEmployee

---
+++ TestingAgain/TestingAgain/HourlyEmployee.cs
@@ -0,0 +1,18 @@
+﻿namespace TestingAgain
+{
+    public class HourlyEmployee : Employee
+    {
+        public float Hours { get; set; }
+        public decimal HourValue { get; set; }
+
+
+        public override decimal GetValueToPay()
+        {
+            return (decimal)Hours * HourValue;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}, \n\t Hours: {Hours:N2}, \n\t Hour Value: {HourValue}, \n\t Value To Pay: {$"{GetValueToPay:C2}",15}  ";
+        }
+    }
+}
